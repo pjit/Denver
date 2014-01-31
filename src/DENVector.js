9015d25a2c1e39cafdecaf061f7558b6fdd16b34
pjit <mydefaultmailid@gmail.com>
2014-01-30 20:46:41 -0500

    First DENVector module

--- src/DENVector.js
+++ src/DENVector.js
@@ -5,3 +5,39 @@
  * Time: 8:19 PM
  * To change this template use File | Settings | File Templates.
  */
+
+//
+//
+//
+var DENVector = (function() {
+    function createVector() {
+        var vector = {};
+
+        vector.x = vector.y = vector.z = 0;
+        if (arguments.length === 3) {
+            vector.x = arguments[0];
+            vector.y = arguments[1];
+            vector.z = arguments[2];
+        }
+        else if (arguments.length == 2) {
+            vector.x = arguments[0];
+            vector.y = arguments[1];
+        }
+        else if (arguments.length == 1) {
+            vector.x = arguments[0];
+        }
+        else {
+            // Nope. Already set to 0
+        }
+
+        vector.toString = function () {
+            return "(" + vector.x + ", " + vector.y + ", " + vector.z + ")";
+        };
+
+        return vector;
+    }
+
+    return {
+        create : createVector
+    };
+}());
