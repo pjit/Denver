ce39cb6f17e58a3e51c4acc260432eda27ad7eb1
pjit <mydefaultmailid@gmail.com>
2014-02-02 09:53:35 -0500

    Particle module

--- src/DENParticle.js
+++ src/DENParticle.js
@@ -5,3 +5,23 @@
  * Time: 12:00 AM
  * To change this template use File | Settings | File Templates.
  */
+
+//
+//
+//
+var DENParticle = (function() {
+   function createParticle() {
+       var particle = {};
+
+       particle.position = DENVector.create(0,0,0);
+       particle.velocity = DENVector.create(0,0,0);
+       particle.acceleration = DENVector.create(0,0,0);
+
+       return particle;
+   }
+
+   return {
+        create : createParticle
+
+   };
+}());
