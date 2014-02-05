96dba9e37e4a8e751bee5b385b00d962f4c4523d
pjit <mydefaultmailid@gmail.com>
2014-02-04 20:06:27 -0500

    Prototype - DENVector and DENParticle

--- src/DENParticle.js
+++ src/DENParticle.js
@@ -9,136 +9,166 @@
 //
 //
 //
-var DENParticle = (function() {
-   function _createParticle() {
-       particle = arguments[0];
-
+   function DENParticle() {
        // Linear position,velocity of the particle in the world space
        // These two properties should not be changed directly - only through
        // integrator but acceleration can be set directly.
-       particle.position = new DENVector.vector(0,0,0);
-       particle.velocity = new DENVector.vector(0,0,0);
+       this.position = new DENVector(0,0,0);
+       this.velocity = new DENVector(0,0,0);
        // Acceleration of the particle. This value can be used to set acceleration due
        // to gravity (its primary use) or any other constant acceleration
-       particle.acceleration = new DENVector.vector(0,-10,0);
+       this.acceleration = new DENVector(0,-10,0);
        // Holds the amount of damping applied to linear motion. Dumping is required
        // to remove energy added through numerical instability in the integrator
-       particle.damping = 0.99;
+       this.damping = 0.99;
        // Holds the inverse of the mass of the particle. It is more useful to hold
        // the inverse mass because integration is simpler and because in real-time
        // simulation i is more useful to have objects with infinite mass (immovable)
        // than zero mass (completely unstable in numerical simulation)
        // zero inverse mass = infinite mass  = immovable objects
        // zero mass = infinite inverse mass.
-       particle.inverseMass = 1;
+       this.inverseMass = 1;
        // Holds accumulated force to be applied at the next simulation iteration only.
        // This value is zeroed at each integration step.
-       particle.forceAccum = new DENVector.vector(0,0,0);
+       this.forceAccum = new DENVector(0,0,0);
+}
 
-       particle.setProperty = function(propertyName, value) {
-           if (value.length == 1) {
-               if (Array.isArray(value[0])) {
-                   var arrayArg = value[0];
+//
+//
+//
+DENParticle.prototype.setProperty = function(propertyName, value) {
+   if (value.length == 1) {
+       if (Array.isArray(value[0])) {
+           var arrayArg = value[0];
 
-                   if (arrayArg.length >= 3) {
-                       particle[propertyName] = new DENVector.vector(arrayArg[0], arrayArg[1], arrayArg[2]);
-                   }
-                   else if (arrayArg.length == 2) {
-                       particle[propertyName] = new DENVector.vector(arrayArg[0], arrayArg[1], 0);
-                   }
-                   else if (arrayArg.length == 1) {
-                       particle[propertyName] = new DENVector.vector(arrayArg[0], 0, 0);
-                   }
-               }
-               else {
-                   if (value[0].x) { // vector ??
-                       particle[propertyName] = new DENVector.vector(value[0].x, value[0].y, value[0].z);
-                   }
-               }
+           if (arrayArg.length >= 3) {
+               this[propertyName] = new DENVector(arrayArg[0], arrayArg[1], arrayArg[2]);
            }
-           else if (value.length == 3) {
-               particle[propertyName].x = value[0];
-               particle[propertyName].y = value[1];
-               particle[propertyName].z = value[2];
+           else if (arrayArg.length == 2) {
+               this[propertyName] = new DENVector(arrayArg[0], arrayArg[1], 0);
            }
-       };
-
-       particle.setPosition = function() {
-            particle.setProperty("position", arguments);
-       };
-       //
-       particle.setVelocity = function() {
-           particle.setProperty("velocity", arguments);
-       };
-       particle.setAcceleration = function() {
-           particle.setProperty("acceleration", arguments);
-       };
-       particle.setDamping = function(damp) {
-            particle.damping = damp;
-       };
-       particle.setInverseMass = function(invMass) {
-            particle.inverseMass = invMass;
-       };
-       particle.setMass = function(mass) {
-            if (mass > 0) {
-                particle.inverseMass = 1/mass;
-            }
-       };
-       // Adds the given force to the particle, to be applied at the next iteration only.
-       particle.addForce = function() {
-           if (arguments.length == 1) {
-               if (arguments[0].x) {// vector ??
-                    particle.forceAccum = new DENVector.vector(particle.forceAccum, arguments[0]);
-               }
+           else if (arrayArg.length == 1) {
+               this[propertyName] = new DENVector(arrayArg[0], 0, 0);
            }
-       };
-       particle.toString = function() {
-            return "position: " + particle.position.toString() + " velocity: " + particle.velocity.toString()
-                + " acceleration: " + particle.acceleration.toString()
-                + " damping: " + particle.damping.toString()
-                + " inverseMass: " + particle.inverseMass.toString()
-                + " Accumulated force: " + particle.forceAccum.toString();
-       };
-       // Integrates the particle forward in time by the given amount.
-       // This function uses a Newton-Euler integration method, which
-       // is a linear approximation of the correct integral. For this
-       // reason it may be inaccurate in some cases.
-       particle.integrate = function() {
-            if (arguments.length == 1) {
-                var duration = arguments[0];
-
-                if (Object.prototype.toString.call(duration).slice(8,-1).toLowerCase()
-                    === "number") {
-                    if (duration > 0) {
-                        var scaledVel = DENVector.scale(particle.velocity, duration);
-
-                        // Update linear position
-                        particle.position = DENVector.add(particle.position, scaledVel);
-
-                        // Acceleration from the force
-                        var scaledAcc = DENVector.scale(particle.forceAccum, particle.inverseMass);
-                        var resultingAcc = DENVector.add(particle.acceleration, scaledAcc);
-                        var acc = DENVector.scale(resultingAcc, duration);
-
-                        particle.velocity = DENVector.add(particle.velocity, acc);
-                        // Impose drag
-                        particle.velocity = DENVector.scale(particle.velocity,
-                            Math.pow(particle.damping, duration));
-                        // Clear the forces
-                        particle.clearAccumulator();
-                    }
-                }
-            }
-       };
-       // clear accumulated force
-       particle.clearAccumulator = function() {
-            particle.forceAccum.init();
-       };
-   }
-
-   return {
-       particle : function particle() {
-            _createParticle(this);
        }
-   };
-}());
+       else {
+           if (value[0].x) { // vector ??
+               this[propertyName] = new DENVector(value[0].x, value[0].y, value[0].z);
+           }
+       }
+   }
+   else if (value.length == 3) {
+       this[propertyName].x = value[0];
+       this[propertyName].y = value[1];
+       this[propertyName].z = value[2];
+   }
+}
+
+//
+//
+//
+DENParticle.prototype.setPosition = function() {
+    this.setProperty("position", arguments);
+}
+
+//
+//
+//
+DENParticle.prototype.setVelocity = function() {
+    this.setProperty("velocity", arguments);
+}
+
+//
+//
+//
+DENParticle.prototype.setAcceleration = function() {
+    this.setProperty("acceleration", arguments);
+}
+
+//
+//
+//
+DENParticle.prototype.setDamping = function(damp) {
+    this.damping = damp;
+}
+
+//
+//
+//
+DENParticle.prototype.setInverseMass = function(invMass) {
+    this.inverseMass = invMass;
+}
+
+//
+//
+//
+DENParticle.prototype.setMass = function(mass) {
+    if (mass > 0) {
+        this.inverseMass = 1/mass;
+    }
+}
+
+//
+// Adds the given force to the particle, to be applied at the next iteration only.
+//
+DENParticle.prototype.addForce = function() {
+   if (arguments.length == 1) {
+       if (arguments[0].x) {// vector ??
+           this.forceAccum = new DENVector(this.forceAccum, arguments[0]);
+       }
+   }
+}
+
+//
+//
+//
+DENParticle.prototype.toString = function() {
+    return "position: " + this.position.toString()
+        + " velocity: " + this.velocity.toString()
+        + " acceleration: " + this.acceleration.toString()
+        + " damping: " + this.damping.toString()
+        + " inverseMass: " + this.inverseMass.toString()
+        + " Accumulated force: " + this.forceAccum.toString();
+}
+
+//
+// Integrates the particle forward in time by the given amount.
+// This function uses a Newton-Euler integration method, which
+// is a linear approximation of the correct integral. For this
+// reason it may be inaccurate in some cases.
+//
+DENParticle.prototype.integrate = function() {
+    if (arguments.length == 1) {
+        var duration = arguments[0];
+
+        if (Object.prototype.toString.call(duration).slice(8,-1).toLowerCase()
+            === "number") {
+            if (duration > 0) {
+                var scaledVel = DENVector.scale(this.velocity, duration);
+
+                // Update linear position
+                this.position = DENVector.add(this.position, scaledVel);
+
+                // Acceleration from the force
+                var scaledAcc = DENVector.scale(this.forceAccum, this.inverseMass);
+                var resultingAcc = DENVector.add(this.acceleration, scaledAcc);
+                var acc = DENVector.scale(resultingAcc, duration);
+
+                this.velocity = DENVector.add(this.velocity, acc);
+                // Impose drag
+                this.velocity = DENVector.scale(this.velocity, Math.pow(this.damping, duration));
+                // Clear the forces
+                this.clearAccumulator();
+            }
+        }
+    }
+}
+
+//
+// clear accumulated force
+//
+DENParticle.prototype.clearAccumulator = function() {
+    this.forceAccum.init();
+}
+
+
--- src/DENVector.js
+++ src/DENVector.js
@@ -7,180 +7,185 @@
  */
 
 //
+// DENVector
+//
+// Constructor. Accepts x,y,z or an array of x,y,z i.e. [x,y,z]
 //
 //
-var DENVector = (function() {
-    function _createVector() {
-        vector = arguments[0];
+function DENVector() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
 
-        vector.x = vector.y = vector.z = 0;
+    // Process any arguments if passed
+    if (arguments.length > 0) {
+        if (arguments.length == 1) {
+            if (Array.isArray(arguments[0])) {
+                var arrayArg = arguments[0];
 
-        if (arguments.length == 2) {
-            valArgs = arguments[1];
-            if (valArgs.length === 3) {
-                vector.x = valArgs[0];
-                vector.y = valArgs[1];
-                vector.z = valArgs[2];
-            }
-            else if (valArgs.length == 2) {
-                vector.x = valArgs[0];
-                vector.y = valArgs[1];
-            }
-            else if (valArgs.length == 1) {
-                vector.x = valArgs[0];
-            }
-            else {
-                // Nope. Already set to 0
-            }
-
-
-
-
-            vector.normalize = function() {
-                var n = vector.magnitude();
-
-                if (n > 0) {
-                    vector.x = vector.x/n;
-                    vector.y = vector.y/n;
-                    vector.z = vector.z/n;
+                if (arrayArg.length == 3) {
+                    this.x = arrayArg[0];
+                    this.y = arrayArg[1];
+                    this.z = arrayArg[2];
                 }
-            };
-            // multiply by scalar
-            vector.multiply = function() {
-                if (arguments.length === 1) {
-                    vector.x *= arguments[0];
-                    vector.y *= arguments[0];
-                    vector.z *= arguments[0];
+                else if (arrayArg.length == 2) {
+                    this.x = arrayArg[0];
+                    this.y = arrayArg[1];
                 }
-            };
+                else if (arrayArg.length == 1) {
+                    this.x = arrayArg[0];
+                }
+            }
+        }
+        else if (arguments.length == 3) {
+            this.x = arguments[0];
+            this.y = arguments[1];
+            this.z = arguments[2];
         }
     }
+}
 
-    function Vector() {
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
+//
+//
+//
+DENVector.prototype.toString = function() {
+  return "(" + this.x + ", " + this.y + ", " + this.z + ")";
+}
 
-        // Process any arguments if passed
-        if (arguments.length > 0) {
-            if (arguments.length == 1) {
-                if (Array.isArray(arguments[0])) {
-                    var arrayArg = arguments[0];
+//
+// Init
+//
+DENVector.prototype.init = function() {
+    this.x = this.y = this.z = 0;
+}
 
-                    if (arrayArg.length == 3) {
-                        this.x = arrayArg[0];
-                        this.y = arrayArg[1];
-                        this.z = arrayArg[2];
-                    }
-                    else if (arrayArg.length == 2) {
-                        this.x = arrayArg[0];
-                        this.y = arrayArg[1];
-                    }
-                    else if (arrayArg.length == 1) {
-                        this.x = arrayArg[0];
-                    }
-                }
-            }
-            else if (arguments.length == 3) {
-                this.x = arguments[0];
-                this.y = arguments[1];
-                this.z = arguments[2];
-            }
-        }
+//
+// Invert
+//
+//
+DENVector.prototype.invert = function() {
+    this.x = -this.x;
+    this.y = -this.y;
+    this.z = -this.z;
+}
+
+//
+// Magnitude of the vector
+//
+DENVector.prototype.magnitude = function() {
+    return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+}
+
+//
+// sqrt is slow on some machines an sometimes we need square only
+//
+DENVector.prototype.squareMagnitude = function() {
+    return (this.x*this.x + this.y*this.y + this.z*this.z);
+}
+
+//
+//
+//
+DENVector.prototype.normalize = function() {
+    var n = this.magnitude();
+
+    if (n > 0) {
+        this.x = this.x/n;
+        this.y = this.y/n;
+        this.z = this.z/n;
     }
-    // Instance methods
-    Vector.prototype.toString = function() {
-      return "(" + this.x + ", " + this.y + ", " + this.z + ")";
-    };
-    // Init
-    Vector.prototype.init = function() {
-        this.x = this.y = this.z = 0;
-    };
-    // Invert
-    Vector.prototype.invert = function() {
-        this.x = -this.x;
-        this.y = -this.y;
-        this.z = -this.z;
-    };
-    // Magnitude of the vector
-    Vector.prototype.magnitude = function() {
-        return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
-    };
+}
 
-    // sqrt is slow on some machines an sometimes we need square only
-    Vector.prototype.squareMagnitude = function() {
-        return (this.x*this.x + this.y*this.y + this.z*this.z);
-    };
-    // Static methods
-    Vector.add = function() {
+//
+// Multiply by scalar
+//
+DENVector.prototype.multiply = function() {
+    if (arguments.length === 1) {
+        this.x *= arguments[0];
+        this.y *= arguments[0];
+        this.z *= arguments[0];
+    }
+}
 
-    };
+//
+// Static methods
+//
 
-    return {Vector: Vector};
+//
+//
+//
+DENVector.add = function() {
+    if (arguments.length == 2) {
+        var v = new DENVector();
 
-    return { // throw this
-        vector : function vector() {
-          // Call internal function to set it
-          _createVector(this, arguments);
-        },
-        add : function() {
-            if (arguments.length == 2) {
-                var v = new DENVector.vector();
+        v.x = (arguments[0].x || 0) + (arguments[1].x || 0);
+        v.y = (arguments[0].y || 0) + (arguments[1].y || 0);
+        v.z = (arguments[0].z || 0) + (arguments[1].z || 0);
 
-                v.x = (arguments[0].x || 0) + (arguments[1].x || 0);
-                v.y = (arguments[0].y || 0) + (arguments[1].y || 0);
-                v.z = (arguments[0].z || 0) + (arguments[1].z || 0);
+        return v;
+    }
+}
 
-                return v;
-            }
-        },
-        // scalarProduct or dotProduct
-        scalarProduct : function() {
-            if (arguments.length == 2) {
-                var result = (arguments[0].x || 0) * (arguments[1].x || 0) +
-                             (arguments[0].y || 0) * (arguments[1].y || 0) +
-                             (arguments[0].z || 0) * (arguments[1].z || 0);
+//
+//
+// scalarProduct or dotProduct
+//
+//
+DENVector.scalarProduct = function() {
+    if (arguments.length == 2) {
+        var result = (arguments[0].x || 0) * (arguments[1].x || 0) +
+            (arguments[0].y || 0) * (arguments[1].y || 0) +
+            (arguments[0].z || 0) * (arguments[1].z || 0);
 
-                return result;
-            }
-        },
+        return result;
+    }
+}
 
-        // vector product or cross product
-        vectorProduct : function() {
-            if (arguments.length == 2) {
-                var v = new DENVector.vector();
+//
+// vector product or cross product
+//
+DENVector.vectorProduct = function() {
+    if (arguments.length == 2) {
+        var v = new DENVector();
 
-                v.x = (arguments[0].y || 0) * (arguments[1].z || 0) -
-                      (arguments[0].z || 0) * (arguments[1].y || 0);
+        v.x = (arguments[0].y || 0) * (arguments[1].z || 0) -
+            (arguments[0].z || 0) * (arguments[1].y || 0);
 
-                v.y = (arguments[0].z || 0) * (arguments[1].x || 0) -
-                      (arguments[0].x || 0) * (arguments[1].z || 0);
+        v.y = (arguments[0].z || 0) * (arguments[1].x || 0) -
+            (arguments[0].x || 0) * (arguments[1].z || 0);
 
-                v.z = (arguments[0].x || 0) * (arguments[1].y || 0) -
-                      (arguments[0].y || 0) * (arguments[1].x || 0);
+        v.z = (arguments[0].x || 0) * (arguments[1].y || 0) -
+            (arguments[0].y || 0) * (arguments[1].x || 0);
 
-                return v;
-            }
-        },
+        return v;
+    }
+}
 
-        normal : function() {
-            if (arguments.length == 1) {
-                var v = new DENVector.vector(arguments[0].x || 0, arguments[0].y || 0, arguments[0].z || 0);
+//
+//
+//
+DENVector.normal = function() {
+    if (arguments.length == 1) {
+        var v = new DENVector(arguments[0].x || 0, arguments[0].y || 0, arguments[0].z || 0);
 
-                v.normalize();
+        v.normalize();
 
-                return v;
-            }
-        },
-        scale : function() {
-            if (arguments.length == 2) {
-                var v = new DENVector.vector(arguments[0].x || 0, arguments[0].y || 0, arguments[0].z || 0);
-                var scaleFactor = arguments[1];
+        return v;
+    }
+}
 
-                v.multiply(scaleFactor);
+//
+//
+//
+DENVector.scale = function() {
+    if (arguments.length == 2) {
+        var v = new DENVector(arguments[0].x || 0, arguments[0].y || 0, arguments[0].z || 0);
+        var scaleFactor = arguments[1];
 
-                return v;
-            }
-        }
-    };
-}());
+        v.multiply(scaleFactor);
+
+        return v;
+    }
+}
+
+
