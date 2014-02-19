1aee0630b807476299db9896ac4c113c9d5214e5
pjit <mydefaultmailid@gmail.com>
2014-02-18 21:19:59 -0500

    DENParticle - JSLinted

--- src/DENParticle.js
+++ src/DENParticle.js
@@ -6,152 +6,174 @@
  * To change this template use File | Settings | File Templates.
  */
 
-"use strict";
+//
+//
+//
+function DENParticle() {
+   'use strict';
 
-//
-//
-//
-   function DENParticle() {
-       // Linear position,velocity of the particle in the world space
-       // These two properties should not be changed directly - only through
-       // integrator but acceleration can be set directly.
-       this.position = new DENVector(0,0,0);
-       this.velocity = new DENVector(0,0,0);
-       // Acceleration of the particle. This value can be used to set acceleration due
-       // to gravity (its primary use) or any other constant acceleration
-       this.acceleration = new DENVector(0,-10,0);
-       // Holds the amount of damping applied to linear motion. Dumping is required
-       // to remove energy added through numerical instability in the integrator
-       this.damping = 0.99;
-       // Holds the inverse of the mass of the particle. It is more useful to hold
-       // the inverse mass because integration is simpler and because in real-time
-       // simulation i is more useful to have objects with infinite mass (immovable)
-       // than zero mass (completely unstable in numerical simulation)
-       // zero inverse mass = infinite mass  = immovable objects
-       // zero mass = infinite inverse mass.
-       this.inverseMass = 1;
-       // Holds accumulated force to be applied at the next simulation iteration only.
-       // This value is zeroed at each integration step.
-       this.forceAccum = new DENVector(0,0,0);
+    // Linear position,velocity of the particle in the world space
+    // These two properties should not be changed directly - only through
+    // integrator but acceleration can be set directly.
+    this.position = new DENVector(0,0,0);
+    this.velocity = new DENVector(0,0,0);
+    // Acceleration of the particle. This value can be used to set acceleration due
+    // to gravity (its primary use) or any other constant acceleration
+    this.acceleration = new DENVector(0,-10,0);
+    // Holds the amount of damping applied to linear motion. Dumping is required
+    // to remove energy added through numerical instability in the integrator
+    this.damping = 0.99;
+    // Holds the inverse of the mass of the particle. It is more useful to hold
+    // the inverse mass because integration is simpler and because in real-time
+    // simulation i is more useful to have objects with infinite mass (immovable)
+    // than zero mass (completely unstable in numerical simulation)
+    // zero inverse mass = infinite mass  = immovable objects
+    // zero mass = infinite inverse mass.
+    this.inverseMass = 1;
+    // Holds accumulated force to be applied at the next simulation iteration only.
+    // This value is zeroed at each integration step.
+    this.forceAccum = new DENVector(0,0,0);
 }
 
 //
 //
 //
 DENParticle.prototype.setProperty = function(propertyName, value) {
-   if (value.length == 1) {
-       if (Array.isArray(value[0])) {
-           var arrayArg = value[0];
+   'use strict';
+
+   if (value.length === 1) {
+       if (Array.isArray(value)) {
+           var arrayArg = value;
 
            if (arrayArg.length >= 3) {
                this[propertyName] = new DENVector(arrayArg[0], arrayArg[1], arrayArg[2]);
            }
-           else if (arrayArg.length == 2) {
+           else if (arrayArg.length === 2) {
                this[propertyName] = new DENVector(arrayArg[0], arrayArg[1], 0);
            }
-           else if (arrayArg.length == 1) {
+           else if (arrayArg.length === 1) {
                this[propertyName] = new DENVector(arrayArg[0], 0, 0);
            }
        }
        else {
-           if (value[0] instanceof DENVector) { // vector ??
-               this[propertyName] = new DENVector(value[0].x, value[0].y, value[0].z);
+           if (value instanceof DENVector) { // vector ??
+               this[propertyName] = new DENVector(value);
            }
        }
    }
-   else if (value.length == 3) {
+   else if (value.length === 3) {
        this[propertyName].x = value[0];
        this[propertyName].y = value[1];
        this[propertyName].z = value[2];
    }
-}
+};
 
 //
 //
 //
 DENParticle.prototype.setPosition = function() {
-    this.setProperty("position", arguments);
-}
+   'use strict';
+
+   this.setProperty("position", arguments);
+};
 
 //
 //
 //
 DENParticle.prototype.setVelocity = function() {
-    this.setProperty("velocity", arguments);
-}
+   'use strict';
+
+   this.setProperty("velocity", arguments);
+};
 
 //
 //
 //
 DENParticle.prototype.setAcceleration = function() {
-    this.setProperty("acceleration", arguments);
-}
+   'use strict';
+
+   this.setProperty("acceleration", arguments);
+};
 
 //
 //
 //
 DENParticle.prototype.setDamping = function(damp) {
+   'use strict';
+
     this.damping = damp || 0.99;
-}
+};
 
 //
 //
 //
 DENParticle.prototype.setInverseMass = function(invMass) {
-    this.inverseMass = invMass || 1;
-}
+   'use strict';
+
+   this.inverseMass = invMass || 1;
+};
 
 //
 //
 //
 DENParticle.prototype.setMass = function(mass) {
+   'use strict';
+
    var m = mass || 0;
 
     if (m > 0) {
         this.inverseMass = 1/m;
     }
-}
+};
 
 //
 //
 //
 DENParticle.prototype.getMass = function() {
-    if (this.inverseMass == 0) {
+   'use strict';
+
+    if (this.inverseMass === 0) {
         return Math.MAX_VALUE;
     }
-    else {
-        return (1.0/this.inverseMass);
-    }
-}
+
+    return (1.0/this.inverseMass);
+};
 
 //
 //
 //
 DENParticle.prototype.hasFiniteMass = function() {
-    return (this.inverseMass != 0);
-}
+   'use strict';
+
+   return (this.inverseMass !== 0);
+};
 
 //
 // Adds the given force to the particle, to be applied at the next iteration only.
 //
 DENParticle.prototype.addForce = function(force) {
+   'use strict';
+
    var f = force || {};
+
    if (f instanceof  DENVector) {
        this.forceAccum.add(f);
    }
-}
+};
 
 //
 //
 //
 DENParticle.prototype.toString = function() {
-    return "position: " + this.position.toString()
-        + " velocity: " + this.velocity.toString()
-        + " acceleration: " + this.acceleration.toString()
-        + " damping: " + this.damping.toString()
-        + " inverseMass: " + this.inverseMass.toString()
-        + " Accumulated force: " + this.forceAccum.toString();
-}
+   'use strict';
+
+   return "position: " + this.position.toString()
+     + " velocity: " + this.velocity.toString()
+     + " acceleration: " + this.acceleration.toString()
+     + " damping: " + this.damping.toString()
+     + " inverseMass: " + this.inverseMass.toString()
+     + " Accumulated force: " + this.forceAccum.toString();
+};
 
 //
 // Integrates the particle forward in time by the given amount.
@@ -160,33 +182,39 @@ DENParticle.prototype.toString = function() {
 // reason it may be inaccurate in some cases.
 //
 DENParticle.prototype.integrate = function(duration) {
-    if (this.inverseMass <= 0) return;
+   'use strict';
 
-     var d = duration || 0;
+   if (this.inverseMass <= 0) {
+      return;
+   }
+   if (duration === undefined || duration === null) {
+      return;
+   }
+   if (duration < 0) {
+      return;
+   }
 
-      if (duration > 0) {
-          var scaledVel = DENVector.scale(this.velocity, duration);
+   var scaledVel = DENVector.scale(this.velocity, duration),
+      // Acceleration from the force
+      scaledAcc = DENVector.scale(this.forceAccum, this.inverseMass),
+      resultingAcc = DENVector.add(this.acceleration, scaledAcc);
 
-          // Update linear position
-          this.position.add(scaledVel);
-
-          // Acceleration from the force
-          var scaledAcc = DENVector.scale(this.forceAccum, this.inverseMass);
-          var resultingAcc = DENVector.add(this.acceleration, scaledAcc);
-
-          this.velocity.add(DENVector.scale(resultingAcc, duration));
-          // Impose drag
-          this.velocity.scale(Math.pow(this.damping, duration));
-          // Clear the forces
-          this.clearAccumulator();
-      }
-}
+   // Update linear position
+   this.position.add(scaledVel);
+   this.velocity.add(DENVector.scale(resultingAcc, duration));
+   // Impose drag
+   this.velocity.scale(Math.pow(this.damping, duration));
+   // Clear the forces
+   this.clearAccumulator();
+};
 
 //
 // clear accumulated force
 //
 DENParticle.prototype.clearAccumulator = function() {
-    this.forceAccum.reset();
-}
+   'use strict';
+
+   this.forceAccum.reset();
+};
 
 
